a9e4860276bd6d4cac149556bdda06bf2a86cc8d
DiegoCSantos <diego.santos@techne.com.br>
2024-08-05 14:20:38 -0300

    initial commmit Project structure

---
+++ src/test/java/br/com/demo/rabbitmq/DemoRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.demo.rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
